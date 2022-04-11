971630daf3e4e74cf87ecdfe25c44c04ccd1bc3f
EugeniaMatto <eugematto26@gmail.com>
2022-04-11 10:24:43 -0700

    exit

--- exit.c
+++ exit.c
@@ -10,13 +10,21 @@ void eexit(char **argv, char *buffer, char *command)
 {
 	int exitstatus = 0;
 
+	if (argv[1] == NULL)
+	{
+		freeMatrix(argv);
+		free(buffer);
+		free(command);
+		exit(0);	
+	}
+
 	if (argv[1] != NULL)
 	{
 		exitstatus = _atoi(argv[1]);
 		if (exitstatus <= 0)
 		{
-			perror(argv[1]);
-			exit(-1);
+			printf("%s: 1: exit: Illegal number: %s\n", argv[0], argv[0]);
+			exit(2);
 		}
 	}
 	freeMatrix(argv);
