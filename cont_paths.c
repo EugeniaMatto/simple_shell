305105e5e03ffbbfee78b345574d9cc737094ec2
trenfranco <trenfranco@gmail.com>
2022-04-11 15:46:22 -0700

    added cont_paths func

---
+++ cont_paths.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * cont_paths - counts paths
+ * @s: string
+ * Return: number of paths
+ */
+int cont_paths(char *s)
+{
+	int i = 0, paths = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (s[i])
+	{
+		if (s[i] == ':')
+			paths++;
+		i++;
+	}
+}
