e8242b485a49013ce734f9893d95c9dfa7e1225b
trenfranco <trenfranco@gmail.com>
2022-04-11 13:39:27 -0700

    added no_space function

---
+++ no_space.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * no_letter - cheks if a string is only spaces
+ * @str: string to compare
+ * Return: 0 if the string is only spaces, 1 if has letters
+ */
+int no_letter(*str)
+{
+	int i = 0, flag = 0;
+
+	while (str[i])
+	{
+		if (str[i] != ' ')
+			flag = 1;
+		i++;
+	}
+	if (flag == 1)
+		return (1);
+
+	return (0);
+
+}
